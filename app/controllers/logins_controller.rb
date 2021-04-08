2b21a8d9f7d5ce144c2c37e315c44ded352eedd7
Wesley Nogueira <wesleysenan@gmail.com>
2021-04-08 00:22:59 -0300

    Adição de logout da aplicação e ajustes de parâmetros para sessão do usuário

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,9 +6,9 @@ class LoginsController < AuthenticationsController
   def create
     if auth?
       @login = Login.new(user_id: @user.first.id)
-      
+
       if @login.save
-        session[:user] = @user
+        session[:user_id] = @user.first.id
         flash[:notice] = 'Login feito com sucesso!'
         redirect_to home_index_path
       else
@@ -18,6 +18,18 @@ class LoginsController < AuthenticationsController
     else
       flash[:error] = 'Nome de usuário ou senha está errado!'
       redirect_to root_path
+    end
+  end
+
+  def destroy
+    @login = Login.find_by(user_id: params[:id])
+
+    if @login.destroy
+      flash[:notice] = 'Saida dos sistema realizada com sucesso!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Não foi possível realizar a saida do sistema!'
+      redirect_to home_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'logins#new'
   
-  resources :logins, only: %i[new create]
+  resources :logins, only: %i[new create destroy]
   resources :authentications, only: %i[new create]
   resources :home, only: %i[index]
 end
